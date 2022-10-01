b63b9c9439cc38b93da6bc1a35fd22e995443782
Harry Graham <hgraham594@gmail.com>
2022-10-01 16:21:23 +0100

    Activate 4th test and make it pass

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,18 @@
 class Bottles
   def verse(n)
-    "#{n} bottles of beer on the wall, " +
-    "#{n} bottles of beer.\n" +
-    "Take one down and pass it around, " +
-    "#{n-1} #{n == 2 ? "bottle" : "bottles"} of beer on the wall.\n"
+    "#{subject(n)} of beer on the wall, " +
+    "#{subject(n)} of beer.\n" +
+    "Take #{n == 1 ? "it" : "one"} down and pass it around, " +
+    "#{subject(n-1)} of beer on the wall.\n"
+  end
+
+  private
+
+  def subject(n)
+    case n
+    when 0 then "no more bottles"
+    when 1 then "1 bottle"
+    else "#{n} bottles"
+    end
   end
 end
