9c7ed3764f8374dcc47b9c3997c36463befa4e1e
Harry Graham <hgraham594@gmail.com>
2022-10-01 16:14:14 +0100

    Activate 2nd & 3rd tests & fix failing 3rd test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,6 +3,6 @@ class Bottles
     "#{n} bottles of beer on the wall, " +
     "#{n} bottles of beer.\n" +
     "Take one down and pass it around, " +
-    "#{n-1} bottles of beer on the wall.\n"
+    "#{n-1} #{n == 2 ? "bottle" : "bottles"} of beer on the wall.\n"
   end
 end
