37b9e31ab4a7a488b49050dfff011103217c6153
Harry Graham <hgraham594@gmail.com>
2022-10-01 16:10:52 +0100

    Fix first test (i.e. add base code for bottles class)

---
+++ lib/bottles.rb
@@ -0,0 +1,8 @@
+class Bottles
+  def verse(n)
+    "#{n} bottles of beer on the wall, " +
+    "#{n} bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "#{n-1} bottles of beer on the wall.\n"
+  end
+end
